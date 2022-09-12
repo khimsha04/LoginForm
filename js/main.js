a9a8d27ff3f9270723d0e6481a227f0b5a929c7e
uknown-me <64226929+uknown-me@users.noreply.github.com>
2022-09-12 21:42:08 +0400

    complete responsive

--- js/main.js
+++ js/main.js
@@ -7,8 +7,23 @@ const errorPassword = document.querySelector(".error-password");
 const updatePassword = document.querySelector(".updatepassword");
 const dontwork = document.querySelector(".dontwork");
 const blackButton = document.querySelector(".black");
+const purpleButton = document.querySelector(".purple");
+const greenButton = document.querySelector(".green");
+const orangeButton = document.querySelector(".orange");
+const yellowButton = document.querySelector(".yellow");
 const body = document.querySelector(".body");
-
+orangeButton.addEventListener("click", function () {
+  body.classList.add("bodyorange");
+});
+yellowButton.addEventListener("click", function () {
+  body.classList.add("bodyyellow");
+});
+greenButton.addEventListener("click", function () {
+  body.classList.add("bodygreen");
+});
+purpleButton.addEventListener("click", function () {
+  body.classList.add("bodypurple");
+});
 blackButton.addEventListener("click", function () {
   body.classList.add("bodyblack");
 });
